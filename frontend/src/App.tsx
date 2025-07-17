03a548648b3798e0c197198bec672fbb141d344f
alexander <alexanderglover478@gmail.com>
2025-07-17 12:02:25 +0000

    This reverts commit e01ca04ad56ac02b34a16f8ce03707e60856fea7.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
+import React from 'react'
+import { Button } from '@mui/material'
 
-import { Box } from '@mui/material'
-// import { SurgeryDashboard } from './Components/Dashboard/SurgeryDashboard'
-import { Dashboard } from './Pages/Dashboard';
 const App = () => {
   return (
-        //  Tanstack query and react router to be added
-    <Box sx={{ minHeight: "100vh", width: "100%" }}>
-      <Box component="main" sx={{ width: "100%" }}>
-        <Dashboard/>
-      </Box>
-    </Box>
-  );
+    <div>
+      <Button variant='contained'>Hello</Button>
+    </div>
+  )
 }
 
 export default App
--- frontend/src/Pages/Dashboard.tsx
+++
@@ -1,6 +0,0 @@
-import { SurgeryDashboard } from '../Components/Dashboard/SurgeryDashboard'
-import React from 'react'
-
-export const Dashboard = () => {
-  return <SurgeryDashboard/>
-}
